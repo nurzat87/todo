fc847b140f0d835b5b8a60979cf08fb20e35ac24
nurzat87 <zusubalievanurzat@gmail.com>
2021-01-14 16:37:41 +0300

    init

--- todo/urls.py
+++ todo/urls.py
@@ -21,7 +21,7 @@ from main.views import homepage, test
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", homepage, name="home"),
-    path("test/",test, name="test"),
+    path("test/", test, name="test"),
    
 
 ]
